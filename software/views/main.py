0bf5f41e7c4469709e20e700c1121702f817b7e9
joaopauloantunes <jpantunesdesouza@gmail.com>
2020-11-27 13:42:35 -0300

    add templates, views, arq txt

--- software/views/main.py
+++ software/views/main.py
@@ -6,7 +6,7 @@ async def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 @app.post("/save_file")
-async def save_file(cores_exclusivas: str):
-    with open('cores_exclusivas.txt', 'w') as arq:  # ruby, gray, blue, red, pink, purple
-        return arq.write(cores_exclusivas) > 0
-    return True
+async def save_file(exclusive_colors: str):
+    with open('exclusive_colors.txt', 'w') as arq:
+        return arq.write(exclusive_colors) > 0
+    return False
